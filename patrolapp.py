f65656087e974b5ae3b85705017670457d7ae477
kwinkunks <matt@agilegeoscience.com>
2013-10-15 15:24:12 -0300

    adding patrol bot and results server

---
+++ patrolapp.py
@@ -0,0 +1,16 @@
+import webapp2
+
+class Handler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello webapp2!")
+        
+app = webapp2.WSGIAppliction([
+    ('/', Handler),
+    ], debug = True)
+    
+def main():
+    from paste import httpserver
+    httpserver.serve(app, host = '127.0.0.1' port = '8081')
+
+if __name__ == '__main__':
+    main()

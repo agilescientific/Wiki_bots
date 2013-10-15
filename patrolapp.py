08d75b9c66f99f19303ed2174c57d5e666eb1560
kwinkunks <matt@agilegeoscience.com>
2013-10-15 15:27:31 -0300

    aaaand making them actually work :)

--- patrolapp.py
+++ patrolapp.py
@@ -2,15 +2,15 @@ import webapp2
 
 class Handler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello webapp2!")
+        self.response.write('Hello webapp2!')
         
-app = webapp2.WSGIAppliction([
+app = webapp2.WSGIApplication([
     ('/', Handler),
     ], debug = True)
     
 def main():
     from paste import httpserver
-    httpserver.serve(app, host = '127.0.0.1' port = '8081')
+    httpserver.serve(app, host = '127.0.0.1', port = '8081')
 
 if __name__ == '__main__':
     main()

fa365a296f341b361e1e3e82bd3a20645b7cdcd6
kwinkunks <matt@agilegeoscience.com>
2013-10-22 11:55:37 -0300

    catedit in decent shape

--- catedit.py
+++ catedit.py
@@ -3,7 +3,10 @@
 
 import mwclient
 from config import config
+import time
 
+##################
+# Authentication
 BOT_NAME = 'exbot'
 PASSWORD = config[BOT_NAME]
 
@@ -14,15 +17,29 @@ wiki = mwclient.Site(WIKI_URL, path=WIKI_PATH)
 wiki.login(BOT_NAME, PASSWORD)
 
 # This is the list of cats we will edit
+# If using 'Recursive' then bot will crawl down subcats
 categories = ['Cat 1', 'Cat A']
 
-#cats_in_wiki = [c.page_title for c in wiki.allcategories()]
+##################
+# OPTIONS
 
+RECURSIVE = True           # Set to True if want to crawl down into subcats
+EDIT_CAT_PAGES = True      # Set to True if you want to edit the cat pages too
+SUMMARY = 'Editing pages'  # Use something useful
+TRIES = 10                 # Number of times to wait for edit conflict
+
+##################
+# Function to make edits
 def edit_page(page):
+    
+    # Keep track of attempts
+    tries = 0
     
     # Do this in a while loop to retry if there's an edit conflict
     while True:
-                
+        
+        tries += 1
+        
         # Grab the text on the page
         text = page.edit()
         
@@ -35,20 +52,27 @@ def edit_page(page):
         
         # Save the page back to the wiki
         try:
-            page.save(text,summary='Adding cat Z')
+            page.save(text,summary=SUMMARY)
+            print 'Processed {0}'.format(page.page_title)
 
-        except EditError:
-            print "-- Failed to save {0}: trying again".format(page.page_title)
-            continue
+        except mwclient.EditError:
+            if tries == TRIES:
+                print "-- Giving up after {0} tries".format(TRIES)
+            else:
+                print "-- Failed to save {0}: trying again".format(page.page_title)
+                time.sleep(2)
+                continue
 
-        except HTTPRedirectError:
+        except mwclient.HTTPRedirectError:
             print "-- Failed to save {0}: redirect error".format(page.page_title)
             
-        except ProtectedPageError:
+        except mwclient.ProtectedPageError:
             print "-- Failed to save {0}: page is protected".format(page.page_title)
 
         break
 
+##################
+# Main loop
 for cat in categories:
     
     for page in wiki.Categories[cat]:
@@ -58,15 +82,14 @@ for cat in categories:
             print "-- Skipping redirect page {0}".format(page.page_title)
             continue
             
-        if page.namespace == 14:
+        if RECURSIVE and (page.namespace == 14):
             # If the page is a category, add it to the end of the list
             # (it's a bit dodgy changing an object we're iterating over, but it works :)
-            print "-- Adding category to list: {0}".format(page.page_title)
-            # Comment this out if you DO NOT want to recusively step over categories
             categories.append(wiki.categories[page.page_title].page_title.encode('utf-8'))
-            # If you want to edit the category pages themselves as well,
-            # COMMENT the next line
-            continue
+            print "-- Added category to list: {0}".format(page.page_title)
+
+            if not EDIT_CAT_PAGES:
+                continue
 
         elif page.namespace != 0:
             # Skip other non-articles
